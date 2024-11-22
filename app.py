a15f66f356f7a58243b7e17ecdbf1ba0365b5ca7
Nikita <nsvep322@gmail.com>
2024-11-22 19:33:24 +0300

    -

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__, static_folder='static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:aboba123@localhost/subsub'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:aboba@amvera-nsvep-cnpg-vsepodpiski-rw/allsub'
 db = SQLAlchemy(app)
 CORS(app)
 migrate = Migrate(app, db)
